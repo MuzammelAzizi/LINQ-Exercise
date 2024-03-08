ebf2eaee632c7e82470a957ed135f047d7261e63
Muzammel Osmani Azizi <ms7.swaggy@gmail.com>
2024-03-07 16:13:50 -0800

    status

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> soccerTeams = new List<string>()
+            {
+                "Real Madrid", "Manchester United", "AC Millan", "Barcelona", "Liverpool", "Ajax", 
+                "Benfica", "Chelsea"
+            };
+
+            var orderedTeams = soccerTeams.OrderBy(name => name.Length);
+            Console.WriteLine("These are the Champions League teams and the names goes by length order");
+
+            foreach (var team in orderedTeams) 
+            {               
+                Console.WriteLine(team);
+            }
+
         }
     }
 }
